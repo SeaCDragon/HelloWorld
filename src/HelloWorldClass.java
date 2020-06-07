963efa8c27d06bd04f942308d9d396de5f060831
Charles McClain <charles.mcclain@hgs.hiddengeniusproject.org>
2020-06-06 18:05:27 -0700

    Initial Commit

---
+++ src/HelloWorldClass.java
@@ -0,0 +1,21 @@
+public class HelloWorldClass {
+    public static void main(String [] args){
+        System.out.println("Hello World");
+    }
+
+    private void meme(String parameter){
+
+    }
+
+    protected void method(){
+
+    }
+
+    void otherMethod(){
+
+    }
+
+    public int intMethod(){
+        return 0;
+    }
+}
